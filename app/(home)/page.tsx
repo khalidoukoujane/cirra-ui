fbe5e21f8755f8925368c68510a7402786e8759d
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-11-30 15:57:29 +0100

    added

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center flex-1 mt-20 gap-5">
-      <div className="w-[60%] flex flex-col items-center justify-center gap-5 ">
+    <div className="flex flex-col items-center flex-1 mt-20 gap-12">
+      <div className="w-[60%] flex flex-col items-center justify-center gap-5">
         <h1 className="text-6xl font-semibold text-center">
           Lightweight UI for creators
         </h1>
@@ -9,8 +11,21 @@ export default function HomePage() {
           Cirra UI blends clean styling with gentle animations to help you build
           polished interfaces with clarity and flow.
         </p>
+        <div className="flex gap-4 mt-4">
+          <Link
+            href="/components/test" //for now
+            className="px-6 py-2.5 bg-foreground text-background rounded-lg font-medium hover:underline duration-200"
+          >
+            Browse Components
+          </Link>
+          <Link
+            href="/components"
+            className="px-6 py-2.5 border border-border rounded-lg font-medium"
+          >
+            Get Started
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
-
